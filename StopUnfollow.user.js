a3d626b007df0c4f7c6ef84f2385d241f2daa8c6
!♥Koͨmͧiͭnͥoͤ Style♥! <91426727+KominoStyle@users.noreply.github.com>
2025-06-08 20:05:03 +0200

    Update StopUnfollow.user.js

--- StopUnfollow.user.js
+++ StopUnfollow.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitch: Stop Unfollow
 // @namespace    http://tampermonkey.net/
-// @version      1.46
+// @version      1.43
 // @description  Inserts “Stop Unfollow” under avatar→Settings. Disables “Unfollow” on saved channels without reloading!
 // @match        https://www.twitch.tv/*
 // @grant        GM_getValue
