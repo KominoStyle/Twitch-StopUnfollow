5958e30ce19cfc8a0c3c96e740f086cf90de7647
!♥Koͨmͧiͭnͥoͤ Style♥! <91426727+KominoStyle@users.noreply.github.com>
2025-06-22 19:25:46 +0200

    Adjust lock icon color

--- StopUnfollow.user.js
+++ StopUnfollow.user.js
@@ -189,7 +189,7 @@
     icon.setAttribute('width', defaultIcon.getAttribute('width') || '20')
     icon.setAttribute('height', defaultIcon.getAttribute('height') || '20')
     icon.setAttribute('viewBox', '0 0 20 20')
-    icon.setAttribute('fill', '#9147ff')
+    icon.setAttribute('fill', '#efeff1')
     icon.title = 'Unfollow disabled for this channel'
     icon.innerHTML = `
       <path fill-rule="evenodd" d="M14.001 5.99A3.992 3.992 0 0 0 10.01 2h-.018a3.992 3.992 0 0 0-3.991 3.99V8H3.999v8a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V8h-1.998V5.99zm-2 2.01V5.995A1.996 1.996 0 0 0 10.006 4h-.01a1.996 1.996 0 0 0-1.995 1.995V8h4z" clip-rule="evenodd"></path>
